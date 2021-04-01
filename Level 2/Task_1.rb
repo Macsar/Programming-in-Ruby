0820e8b020645e72b7ba6dcb3253f000ceab48ef
Max <limas-m@yandex.ru>
2021-04-01 15:17:46 +1000

    Готово

--- Level 2/Task_1.rb
+++ Level 2/Task_1.rb
@@ -5,3 +5,64 @@
 # язык выражение “expose the state” не получилось. Посмотрите перевод слова “expose” в
 # словаре). Создайте экземпляр класса Monkey, вызовите методы объекта и проверьте
 # работоспособность программы.
+
+class Monkey
+  def initialize(options = {})
+    @name = options[:name]
+    @age = options[:age]
+    @play = false
+  end
+
+  attr_reader :name, :age, :play
+
+  # обезьянка начинает играть
+  def run
+    @play = true
+  end
+
+  # обезьянка прекращает играть
+  def stop
+    @play = false
+  end
+
+  # карточка обезьянки
+  def cart
+    puts 'Карточка обезьянки:'
+    puts "Имя: #{@name}"
+    puts "Возраст: #{@age}"
+
+    if @play
+      puts 'Обезьянка в данный момент играет.'
+    else
+      puts 'Обезьянка в данный момент не играет.'
+    end
+  end
+end
+
+monkey = Monkey.new(name: 'Max', age: 26)
+
+loop do
+  monkey.cart
+
+  if monkey.play
+    puts 'Остановить обезьянку? (да/нет)'
+    monkey_stop = gets.chomp
+
+    case monkey_stop
+    when 'да'
+      monkey.stop
+    else
+      exit
+    end
+  else
+    puts 'Поиграть с обезьянкой? (да/нет)'
+    monkey_stop = gets.chomp
+
+    case monkey_stop
+    when 'да'
+      monkey.run
+    else
+      exit
+    end
+  end
+end
