5b41954d03fce6fa8acb1fe00ecbb187211c529d
Max <limas-m@yandex.ru>
2021-04-06 22:08:53 +1000

    Update

---
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./1/conditional.rb
@@ -0,0 +1,27 @@
+if 2 < 1
+  puts 'yes'
+else
+  puts 'no'
+end
+
+# обратный if
+puts 'yes' unless 2 < 1
+puts 'yes' if 2 < 1
+
+name = 'Masha'
+
+if name == 'Dasha'
+  puts 'My name is ' + name
+elsif name == 'Pasha'
+  puts 'My name is ' + name
+elsif name == 'Masha'
+  puts 'My name is ' + name
+end
+
+if name == 'Masha' && 1 < 2
+  puts 'My name is ' + name
+end
+
+if name == 'Masha' || 1 < 2 && name != 'Dasha'
+  puts 'My name is ' + name
+end
