af41c84f21796519a7f1dfe0d035fc97e79e392a
Max <limas-m@yandex.ru>
2021-04-07 15:14:26 +1000

    15 урок готов

--- RUBY - 2020. С Нуля и до Профи. Полный курс./2/item.rb
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./2/item.rb
@@ -1,14 +1,8 @@
 class Item
-  def initialize(options)
+  attr_accessor :price, :weight
+
+  def initialize(options = {})
     @price = options[:price]
     @weight = options[:weight]
   end
-
-  attr_accessor :price, :weight
 end
-
-item1 = Item.new({ weight: 20, price: 50 })
-item2 = Item.new({ weight: 30, price: 60 })
-
-p item1
-p item2
