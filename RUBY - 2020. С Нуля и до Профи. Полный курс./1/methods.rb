a3012e895ff721ff65b7acabd7c7c4c112768a61
Max <limas-m@yandex.ru>
2021-04-07 14:50:00 +1000

    Update

--- RUBY - 2020. С Нуля и до Профи. Полный курс./1/methods.rb
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./1/methods.rb
@@ -1,16 +1,11 @@
-def division_1
-  puts 'method body'
-end
-
-def division_2(name)
+def division_2(name = '', age = nil, weight = 71)
   puts 'My name is ' + name
+  puts 'My age is ' + age.to_s
+  puts 'My weight is ' + "#{weight}"
+  puts '_____'
 end
 
-def division_3
-  name = 'Dasha'
-
-  division_1
-  division_2(name)
-end
-
-division_3
+division_2('Dasha', 21)
+division_2('Pasha', 25, 78)
+division_2('Sasha', 35, 70)
+division_2 'Anton'
