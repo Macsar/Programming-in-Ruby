f2370580d00329cdff7ba3cc44af3190ea1ebd43
Max <limas-m@yandex.ru>
2021-04-08 00:29:21 +1000

    17 урок готов

--- RUBY - 2020. С Нуля и до Профи. Полный курс./2/init.rb
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./2/init.rb
@@ -1,11 +1,8 @@
 require_relative 'cart'
 require_relative 'item'
 
-cart = Cart.new
-cart.add_item(Item.new({ price: 200 }))
-cart.add_item(Item.new)
-cart.validate
+item = Item.new({ price: 25, weight: 120, name: 'Car' })
+item1 = Item.new({ price: 25, weight: 120, name: 'Car' })
 
-cart.remove_item
-
-p cart
+item.info { |value| puts value }
+item1.info
--- RUBY - 2020. С Нуля и до Профи. Полный курс./2/item.rb
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./2/item.rb
@@ -1,8 +1,21 @@
 class Item
-  attr_accessor :price, :weight
+  attr_accessor :price, :weight, :name
 
   def initialize(options = {})
     @price = options[:price]
     @weight = options[:weight]
+    @name = options[:name]
+  end
+
+  def info
+    # проверка на то, передан ли блок
+    if block_given?
+      # yield - генератор. Например, он выполняет блок, переданный ему
+      yield price
+      yield weight
+      yield name
+    else
+      puts 'Nothing to show'
+    end
   end
 end
