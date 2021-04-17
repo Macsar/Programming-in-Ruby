ce0e8f60e634d52f04246d72b4e57281740fe230
Max <limas-m@yandex.ru>
2021-04-18 08:10:31 +1000

    Update

--- RUBY - 2020. С Нуля и до Профи. Полный курс./2/init.rb
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./2/init.rb
@@ -8,23 +8,30 @@
 # Подключает все файлы .rb в данном каталоге
 Dir['*.rb'].each { |file| require_relative file unless file == 'init.rb' }
 
-item1 = VirtualItem.new({ price: 25.0, weight: 120, name: 'Car' })
-item2 = RealItem.new({ weight: 120.0, name: 'Car' })
-item3 = RealItem.new({ weight: 120.0, name: 'Bike' })
+item1 = VirtualItem.new({ price: 202.0, weight: 120, name: 'Car' })
+item2 = RealItem.new({ price: 202.0, weight: 220.0, name: 'Cycle' })
+item3 = RealItem.new({ price: 90.0, weight: 320.0, name: 'Bike' })
 
 cart = Cart.new
 cart.add_item item1
 cart.add_item item2
-cart.remove_item
-p cart.items.size
+cart.add_item item3
 
-order = Order.new
-order.add_item item1
-order.add_item item2
-order.add_item item3
-order.remove_item
-p order.items.size
-p order.count_valid_items
+p cart
+
+# cart = Cart.new
+# cart.add_item item1
+# cart.add_item item2
+# cart.remove_item
+# p cart.items.size
+#
+# order = Order.new
+# order.add_item item1
+# order.add_item item2
+# order.add_item item3
+# order.remove_item
+# p order.items.size
+# p order.count_valid_items
 
 # p item1.price
 # p item1.real_price
--- RUBY - 2020. С Нуля и до Профи. Полный курс./2/item_container.rb
+++ RUBY - 2020. С Нуля и до Профи. Полный курс./2/item_container.rb
@@ -1,26 +1,45 @@
 module ItemContainer
-  def add_item(item)
-    @items.push item
-  end
-
-  def remove_item
-    @items.pop
-  end
-
-  def validate
-    @items.each do |i|
-      # чекаем на наличие прайса
-      puts 'Item has no price' if i.price.nil?
+  module ClassMethods
+    MIN_PRICE = 100
+    # метод, что бы могли его вызывать без создания объекта
+    # Называется Статическим Методом
+    def min_price
+      MIN_PRICE
     end
   end
 
-  # Метод для удаления item, которые не содержат цены
-  def delete_invalid_items
-    @items.delete_if { |i| i.price.nil? }
+  # инстанстные методы - это методы, оздаваемый через объект
+  module InstanceMethods
+    def add_item(item)
+      @items.push item unless item.price < self.class.min_price
+    end
+
+    def remove_item
+      @items.pop
+    end
+
+    def validate
+      @items.each do |i|
+        # чекаем на наличие прайса
+        puts 'Item has no price' if i.price.nil?
+      end
+    end
+
+    # Метод для удаления item, которые не содержат цены
+    def delete_invalid_items
+      @items.delete_if { |i| i.price.nil? }
+    end
+
+    # Метод возвращает те товары, у которых есть цена
+    def count_valid_items
+      @items.count { |i| i.price }
+    end
   end
 
-  # Метод возвращает те товары, у которых есть цена
-  def count_valid_items
-    @items.count { |i| i.price }
+  # коллбэк для подключения внутренних модулей
+  # название included может быть любым
+  def self.included(classes)
+    classes.extend ClassMethods
+    classes.class_eval { include InstanceMethods }
   end
 end
